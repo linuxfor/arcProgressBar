a27f8e5d894c18df2e529f11e6bc9e5ab0620c13
Rainbow888 <1099634723@qq.com>
2016-05-18 10:55:52 +0800

    first commit

---
+++ app/src/main/java/com/qhung/customview/MainActivity.java
@@ -0,0 +1,25 @@
+package com.qhung.customview;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import com.qhung.library.widget.CircleProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+   private  CircleProgressBar circleProgressBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        circleProgressBar = (CircleProgressBar) findViewById(R.id.progressbar);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                circleProgressBar.setMaxValue(90);
+                circleProgressBar.setProgress(90);
+            }
+        },1000);
+
+    }
+}
